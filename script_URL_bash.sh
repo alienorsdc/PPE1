7337c1aa67e3208afda6dd55ff2b8d55f125e955
alienorsdc <114561783+alienorsdc@users.noreply.github.com>
2022-11-09 11:21:51 +0100

    Update script_URL_bash.sh

--- script_URL_bash.sh
+++ script_URL_bash.sh
@@ -15,7 +15,7 @@ fichier_tableau=$2 # le fichier HTML en sortie
 # !!!!!!
 # ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
 
-if [ $# -ne 2] # Si le nb d'argument est différent de 2 alors : (avec ne = not equal)
+if [ $# -ne 2 ] # Si le nb d'argument est différent de 2 alors : (avec ne = not equal)
 then
     echo "Ce programme demande deux fichiers en argument."
     exit
@@ -36,7 +36,7 @@ lineno=1;
 
 while read -r line;
 do
-
+echo
 done < $fichier_urls
 
 while read -r line;
